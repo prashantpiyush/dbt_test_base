d0b564e33fb4d96c93b8051345548ef5555ea577
piyush.5862@gmail.com <piyush.5862@gmail.com>
2024-09-03 07:58:51 +0000

    🔁 Modified     - Test        - custom_unique        - min_value_test

--- base/tests/generic/min_value_test.sql
+++ base/tests/generic/min_value_test.sql
@@ -1,6 +1,6 @@
 {% test min_value_test(model, column_name, threshold) %}
 
-select * from {{model}} where column_name < {{threshold}}
+select * from {{ model }} where column_name < {{ threshold }}
 
 {% endtest %}
 
