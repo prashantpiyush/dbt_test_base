a0d315d633f96de673bfa20053ae64c72bd442e5
piyush.5862@gmail.com <piyush.5862@gmail.com>
2024-09-04 21:03:35 +0000

    ❇️ New     - Test        - max_value_test_with_extra_col
    🔁 Modified
        - Model
           - orders
           - use_model
        - Test
           - max_value_test

--- dep/tests/generic/max_value_test.sql
+++ dep/tests/generic/max_value_test.sql
@@ -1,4 +1,10 @@
 {% test max_value_test(model, column_name, threshold) %}
+
+select * from {{ model }} where {{ column_name }} > {{ threshold }}
+{% endtest %}
+
+ {% test max_value_test_with_extra_col(model, column_name, ig_col, threshold) %}
+
 select * from {{ model }} where {{ column_name }} > {{ threshold }}
 {% endtest %}
 
