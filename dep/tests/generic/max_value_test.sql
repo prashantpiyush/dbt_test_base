23f4ab08880e10c478f09d5a98f1efc245245e90
piyush.5862@gmail.com <piyush.5862@gmail.com>
2024-09-20 08:13:36 +0000

    ❌ Deleted     - Source        - hive_metastore.pp_test
    🔁 Modified
        - Test
           - max_value_test
        - Model
           - orders
        - Function/Gem
           - generate_schema_name

--- dep/tests/generic/max_value_test.sql
+++ dep/tests/generic/max_value_test.sql
@@ -1,7 +1,7 @@
 {% test max_value_test(model, column_name, threshold) %}
 
-select * from {{ model }} where {{ column_name }} > {{ threshold }}
 
+select * from {{ model }} where {{ column_name }} > {{ threshold }}
 {% endtest %}
 
  
