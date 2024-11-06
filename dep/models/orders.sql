792dc8682d96581b1673194ac4a0cab2714502b9
piyush.5862@gmail.com <piyush.5862@gmail.com>
2024-11-06 05:31:47 +0000

    New     - Source        - hive_metastore.qa_database     - Seed        - raw_payments
    Modified
        - Model
           - orders
        - Seed
           - raw_orders

--- dep/models/orders.sql
+++ dep/models/orders.sql
@@ -1,6 +1,6 @@
 {{
   config({    
-    "schema": "pp_test"
+    "materialized": "table"
   })
 }}
 
@@ -10,8 +10,33 @@ WITH raw_orders AS (
   
   FROM {{ ref('raw_orders')}}
 
+),
+
+raw_payments AS (
+
+  SELECT * 
+  
+  FROM {{ ref('raw_payments')}}
+
+),
+
+Join_1 AS (
+
+  {#Combines order details with payment information for comprehensive order tracking.#}
+  SELECT 
+    in0.id AS id,
+    in0.user_id AS user_id,
+    in0.order_date AS order_date,
+    in0.status AS status,
+    in1.payment_method AS payment_method,
+    in1.amount AS amount
+  
+  FROM raw_orders AS in0
+  INNER JOIN raw_payments AS in1
+     ON in1.order_id = in0.id
+
 )
 
 SELECT *
 
-FROM raw_orders
+FROM Join_1
